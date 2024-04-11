77ef8a662254463fb8c5cff6321cd355d7be57a4
Alexander <sasacihun@gmail.com>
2024-04-11 13:04:28 +0300

    feat: Create working basic page

---
+++ task_manager/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+from task_manager.views import index
+
+urlpatterns = [
+    path("", index, name='index'),
+]
