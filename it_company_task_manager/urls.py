f16eb0d1fc3e45caefb979f1cb1d3e920a032fe4
Alexander <sasacihun@gmail.com>
2024-04-12 18:29:47 +0300

    feat: Add login

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -21,4 +21,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("task_manager.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
