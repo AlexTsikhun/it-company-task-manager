2563d7937e6f16600012ef00d6fe78e62d65d05e
Alexander <sasacihun@gmail.com>
2024-04-25 11:31:14 +0300

    fix: Remove redundant code from installed lib

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
     # path("accounts/", include("django.contrib.auth.urls")),
-    path('', include('admin_star.urls')),
+    # path('', include('admin_star.urls')),
 ]
